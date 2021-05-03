7923d1e52301c8f9e18ba3a1adef68f0fc1253c4
Denis Lopatin <stalk.sdv@gmail.com>
2021-05-03 17:00:25 +0200

    Sign Up form - added handlers for emailrd inputs

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,32 @@
+import { useState } from 'react'
 export default () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(email, password)
+  }
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className='for-group'>
         <label>Email Address</label>
-        <input type='text' className='form-control'></input>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          type='text'
+          className='form-control'
+        ></input>
       </div>
       <div className='for-group'>
         <label>Password</label>
-        <input type='password' className='form-control'></input>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type='password'
+          className='form-control'
+        ></input>
       </div>
       <button className='btn btn-primary'>Sign Up</button>
     </form>
