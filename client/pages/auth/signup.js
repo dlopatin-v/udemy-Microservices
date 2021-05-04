6a71f9ce1a9229ebca68dd27b04c0ae74ba64bf4
Denis Lopatin <stalk.sdv@gmail.com>
2021-05-04 22:39:40 +0200

    Sign up page - Added an onSuccess callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import axios from 'axios'
+import Router from 'next/router'
 import useRequest from '../../hooks/use-request'
 
 export default () => {
@@ -11,12 +11,13 @@ export default () => {
     body: {
       email,
       password
-    }
+    },
+    onSuccess: () => Router.push('/')
   })
 
   const onSubmit = async e => {
     e.preventDefault()
-    doRequest()
+    await doRequest()
   }
   return (
     <form onSubmit={onSubmit}>
